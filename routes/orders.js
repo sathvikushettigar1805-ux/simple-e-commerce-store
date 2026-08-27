1be18af2b52918f4841a9cc7c90837adde7b1b95
Sathvik U Shettigar <sathvikushettigar1805@gmail.com>
2026-08-27 12:05:04 +0530

    Fix Order model filename case

--- routes/orders.js
+++ routes/orders.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Order = require("../models/order");
+const Order = require("../models/Order");
 
 
 // =============================
